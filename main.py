f921b3edc651e9a791d273ae71bd5c5bbe55db69
SheppardJakuard <fa.mo@gmx.de>
2025-08-30 19:19:42 +0200

    0.01 added path based book readout

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from stats import get_book_text, word_count, char_count, sorting, print_sorted_list
+import sys
 
 def main():
-	text = get_book_text("books/frankenstein.txt")
+	if len(sys.argv) < 2:
+		print("Usage: python main.py <file-path>")
+		sys.exit(1)
+	text = get_book_text(sys.argv[1])
 	print("============ BOOKBOT ============")
-	print("Analyzing book found at books/frankenstein.txt")
+	print("Analyzing book found at {sys.argv[1]}")
 	print(word_count(text))
 	print("--------- Character Count -------")
 	print_sorted_list(sorting(char_count(text)))
+	sys.exit(0)
 	
 main()
